f49ffb7428a5ffcb7cd63259404acdb128126e05
OnizukaHime07 <84855627+OnizukaHime07@users.noreply.github.com>
2021-06-07 08:26:46 +0530

    my dictionary

---
+++ dictionary.py
@@ -0,0 +1,14 @@
+dict ={}
+my_dict = {
+    "name": "Rucha",
+    "age":   23,
+    "state":  "Maharashtra",
+    "city":  "Nagpur",
+    "no":    59,
+
+} 
+print(my_dict)
+my_dict.update(street = '34')
+print(my_dict)
+my_dict["rank"] = "left"
+print(my_dict)
