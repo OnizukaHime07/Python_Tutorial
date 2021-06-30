b93e051179fe2aabaea3beebe0c7ca1c3896d500
OnizukaHime07 <84855627+OnizukaHime07@users.noreply.github.com>
2021-06-30 12:22:12 +0530

    file

---
+++ writefile.py
@@ -0,0 +1,11 @@
+writefile= open('sample.txt', 'w')
+writefile.write("We are one\n")
+writefile.close()
+
+writefile= open('sample.txt','a')
+writefile.write("give hope to lie\n")
+writefile.close()
+
+writefile= open('sample.txt','a')
+writefile.write("ruskin bond")
+writefile.close()
