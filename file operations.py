bbc9b4b024e58843b8023c52d7d2869cccc1a5ea
OnizukaHime07 <84855627+OnizukaHime07@users.noreply.github.com>
2021-06-30 12:03:37 +0530

    Bull

--- file operations.py
+++ file operations.py
@@ -1,3 +1,9 @@
 file= open('../POC list.txt','r')
 for each in file:
     print(each)
+
+file1= open('../POC list.txt', 'r')
+print(file1.read(150))
+
+file1= open('../POC list.txt', 'r')
+print(file1.read())
